68bba76c5f9ef9dca100d72019164c69cb31892f
Catalina <cata.cordoba8@gmail.com>
2020-01-26 22:42:11 -0500

    login y paginas

--- eventosApp/eventos/urls.py
+++ eventosApp/eventos/urls.py
@@ -8,8 +8,14 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^events/$', views.events_view, name='events'),
     url(r'^events/(?P<event_id>\d+)/$', views.events_view, name='event'),
-    url(r'^addUser/$', views.add_user_view, name='addUser'),
-    url(r'^login/$', views.login_view, name='login'),
+    url(r'^addUser', views.add_user_view, name='addUser'),
+    url(r'^registerUser', views.register_user_view, name='registerUser'),
+    url(r'^login', views.login_view, name='login'),
     url(r'^isLogged/$', views.is_logged_view, name='isLogged'),
     url(r'^addNewEvent', views.add_new_event, name='addNewEvent'),
+    url(r'^editEvent/(?P<event_id>\d+)/$', views.edit_event, name='editEvent'),
+    # url(r'^viewEvent/(?P<event_id>\d+)/$', views.view_event, name='viewEvent'),
+    url(r'^loginUser', views.login_user, name='loginUser'),
+
+
 ]
--- eventosApp/eventos/views.py
+++ eventosApp/eventos/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth import authenticate, login
 from django.contrib.auth.models import User
 from django.http import HttpResponse, JsonResponse, HttpResponseRedirect
 from django.core import serializers
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
 from django.urls import reverse
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.authentication import TokenAuthentication
@@ -21,7 +21,14 @@ def index(request):
     return render(request, 'eventos/index.html', context)
 
 
-@api_view(['GET', 'POST', 'PUT', 'DELETE'])
+@csrf_exempt
+def edit_event(request, event_id=None):
+    event = Event.objects.get(id=event_id)
+    context = {'event': event}
+    return render(request, 'eventos/edit_event_form.html', context)
+
+
+@api_view(['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'])
 # @authentication_classes([TokenAuthentication])
 # @permission_classes([IsAuthenticated])
 @csrf_exempt
@@ -29,7 +36,9 @@ def events_view(request, event_id=None):
     if request.method == 'GET':
         events_list = Event.objects.order_by('-created_date')
         if event_id is not None:
-            events_list = events_list.filter(id=event_id)
+            event = Event.objects.get(id=event_id)
+            context = {'event': event}
+            return render(request, 'eventos/event_detail.html', context)
         return HttpResponse(serializers.serialize("json", events_list), content_type="application/json")
     elif request.method == 'POST':
         virtual = False
@@ -55,6 +64,8 @@ def events_view(request, event_id=None):
                                                          endDate=event_data['endDate'], virtual=event_data['virtual'])
         event = Event.objects.filter(id=event_data['id'])
         return HttpResponse(serializers.serialize("json", event), content_type="application/json")
+    elif request.method == 'OPTIONS':
+        return Response({'message': 'OK'})
     else:
         Event.objects.filter(id=event_id).delete()
         return Response({'message': 'deleted'})
@@ -62,36 +73,36 @@ def events_view(request, event_id=None):
 
 @csrf_exempt
 def add_user_view(request):
-    if request.method == 'POST':
-        jsonUser = json.loads(request.body)
-        password = jsonUser['password']
-        email = jsonUser['email']
+    jsonUser = json.loads(request.body)
+    password = jsonUser['password']
+    email = jsonUser['email']
 
-        user_model = User.objects.create_user(email=email, password=password)
-        user_model.email = email
-        user_model.save()
+    user_model = User.objects.create_user(username=email, password=password)
+    user_model.first_name = email
+    user_model.last_name = email
+    user_model.email = email
+    user_model.save()
     return HttpResponse(serializers.serialize("json", [user_model]))
 
 
 @csrf_exempt
 def login_view(request):
     if request.method == 'POST':
-        jsonUser = json.loads(request.body)
-        email = jsonUser['email']
-        password = jsonUser['password']
-        user = authenticate(email=email, password=password)
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        user = authenticate(username=email, password=password)
         if user is not None:
             login(request, user)
             message = "ok"
         else:
             message = 'Email o contraseña incorrectos'
-
-    return JsonResponse({"message": message})
-
+        return JsonResponse({"message": message})
+    else:
+        return render(request, "eventos/login.html")
 
 @csrf_exempt
 def is_logged_view(request):
-    if request.user.is_authenticated():
+    if request.user.is_authenticated:
         message = 'ok'
     else:
         message = 'no'
@@ -102,3 +113,13 @@ def is_logged_view(request):
 @csrf_exempt
 def add_new_event(request):
     return render(request, "eventos/event_form.html")
+
+
+@csrf_exempt
+def login_user(request):
+    return render(request, "eventos/login.html")
+
+
+@csrf_exempt
+def register_user_view(request):
+    return render(request, "eventos/register.html")
